3c511b0638c1a5506530a00bdc5836490a59c437
Mirzet Murtić <mirzinho@gmail.com>
2022-04-23 14:51:18 +0200

    Sidebar, fonts

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface SidebarItem {
+    icon: string;
+    name: string;
+}
+
 @Component({
     selector: 'app-sidebar',
     templateUrl: './sidebar.html',
     styleUrls: ['./sidebar.scss']
 })
 export class SidebarComponent {
+    public sidebarItems: Array<SidebarItem> = [
+        { icon: 'users', name: 'Characters' },
+        { icon: 'film', name: 'Movies' }
+    ];
     constructor() {}
 }
